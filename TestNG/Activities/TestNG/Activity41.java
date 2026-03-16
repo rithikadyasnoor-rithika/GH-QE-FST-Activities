ce6ade54cac236ba6fb425214105430c1b8ca2e8
rithikadyasnoor-rithika <rithikadyasnoor@gmail.com>
2026-03-16 16:45:59 +0530

    Add files via upload

---
+++ TestNG/Activities/TestNG/Activity41.java
@@ -0,0 +1,9 @@
+package TestNG;
+import org.testng.annotations.Test;
+
+public class Activity41{
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
